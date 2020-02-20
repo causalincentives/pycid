3d39d7b18fce78fa7615f4d99c28003ce3874e54
RyanCarey <ry.duff@gmail.com>
2020-02-20 13:48:48 +0000

    cid class

---
+++ cid.py
@@ -0,0 +1,92 @@
+from pgmpy.factors.base import BaseFactor
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import (
+    TabularCPD,
+    JointProbabilityDistribution,
+    DiscreteFactor,
+)
+from pgmpy.factors.continuous import ContinuousFactor
+
+
+class NullCPD(BaseFactor):
+    def __init__(self, variable):
+        self.variable = variable
+
+    def scope(self):
+        return [self.variable]
+
+
+class CID(BayesianModel):
+    def __init__(self, ebunch=None):
+        super(CID, self).__init__(ebunch=ebunch)
+
+    def check_sufficient_recall(self):
+        pass
+
+    def solve(self, decision_name):
+        #returns an optimal cpd for a given decision
+        pass
+
+    def expected_utility(self, cpd_dict):
+        #input a cpd for each decision
+        pass
+
+    def add_cpds(self, *cpds):
+        for cpd in cpds:
+            if not isinstance(cpd, (TabularCPD, ContinuousFactor, NullCPD)):
+                raise ValueError("Only TabularCPD, ContinuousFactor, or NullCPD can be added.")
+
+            if set(cpd.scope()) - set(cpd.scope()).intersection(set(self.nodes())):
+                raise ValueError("CPD defined on variable not in the model", cpd)
+
+            for prev_cpd_index in range(len(self.cpds)):
+                if self.cpds[prev_cpd_index].variable == cpd.variable:
+                    logging.warning(
+                        "Replacing existing CPD for {var}".format(var=cpd.variable)
+                    )
+                    self.cpds[prev_cpd_index] = cpd
+                    break
+            else:
+                self.cpds.append(cpd)
+
+    def check_model(self, allow_null=False):
+        """
+        Check the model for various errors. This method checks for the following
+        errors.
+
+        * Checks if the sum of the probabilities for each state is equal to 1 (tol=0.01).
+        * Checks if the CPDs associated with nodes are consistent with their parents.
+
+        Returns
+        -------
+        check: boolean
+            True if all the checks are passed
+        """
+        for node in self.nodes():
+            cpd = self.get_cpds(node=node)
+
+            if cpd is None:
+                raise ValueError("No CPD associated with {}".format(node))
+            elif isinstance(cpd, (TabularCPD, ContinuousFactor)):
+                evidence = cpd.get_evidence()
+                parents = self.get_parents(node)
+                if set(evidence if evidence else []) != set(parents if parents else []):
+                    raise ValueError(
+                        "CPD associated with {node} doesn't have "
+                        "proper parents associated with it.".format(node=node)
+                    )
+                if not cpd.is_valid_cpd():
+                    raise ValueError(
+                        "Sum or integral of conditional probabilites for node {node}"
+                        " is not equal to 1.".format(node=node)
+                    )
+            elif isinstance(cpd, (NullCPD)):
+                if not allow_null:
+                    raise ValueError(
+                        "CPD associated with {node} is nullcpd".format(node=node)
+                    )
+        return True
+
+
+
+    
