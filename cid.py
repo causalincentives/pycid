df8d5fd9b2b57a81e56f326313711792e4ff0013
RyanCarey <ry.duff@gmail.com>
2020-03-02 17:52:46 +0000

    apache license

--- cid.py
+++ cid.py
@@ -1,3 +1,6 @@
+#Licensed to the Apache Software Foundation (ASF) under one or more contributor license 
+#agreements; and to You under the Apache License, Version 2.0.
+
 import numpy as np
 from pgmpy.factors.base import BaseFactor
 from pgmpy.models import BayesianModel
