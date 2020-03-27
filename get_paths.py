a28c5bf4e6c0bfc0dab71ac04c4d771faa970388
RyanCarey <ry.duff@gmail.com>
2020-03-27 14:38:24 +0000

    _get_path_pair working

--- get_paths.py
+++ get_paths.py
@@ -5,19 +5,19 @@ from typing import List
 import numpy as np
 
 
-def _find_path_recurse(bn, path: List[str], B: str):
+def _find_dirpath_recurse(bn, path: List[str], B: str):
     if path[-1]==B:
         return path
     else:
         children = bn.get_children(path[-1])
         for child in children:
             ext = path + [child]
-            ext = _find_path_recurse(bn, ext, B)
+            ext = _find_dirpath_recurse(bn, ext, B)
             if ext and ext[-1]==B:
                 return ext
 
-def find_path_from(bn, A, B):
-    return _find_path_recurse(bn, [A], B)
+def find_dirpath(bn, A, B):
+    return _find_dirpath_recurse(bn, [A], B)
 
 def _active_neighbours(bn, path: List[str], E: List[str]):
     #find possibly active extensions of path conditional on E
@@ -39,7 +39,7 @@ def _active_neighbours(bn, path: List[str], E: List[str]):
     return new_active_neighbours
     
 
-def find_active_path_recurse(bn, path, B, E):
+def find_active_path_recurse(bn, path:List, B:str, E:List):
     #find active path from `path' to `B' given `E'
     if path[-1]==B and B not in E:
         return path
@@ -54,12 +54,25 @@ def find_active_path_recurse(bn, path, B, E):
 #def find_active_path(bn, A, B, E):
 #    return _find_active_path_recurse(bn, [A], B, E)
 
-def _get_path_pair(cid, X, D):
-    for utility in cid.utilities:
-        control_path = find_path_from(cid, D, utility)
-        other_parents = [i for i in cid.get_parents(D) if i!=X]
+def _get_path_pair(cid, D, X):
+    assert (X,D) in cid.edges, '{} is not a parent of {}'.format(X, D)
+    #remove observations of previous decisions
+    cid = cid.copy()
+    decisions = cid._get_valid_order(cid._get_decisions())
+    ind = np.argmax(np.array(decisions)==D)
+    prior_decisions = decisions[:ind]
+    for edge in list(cid.edges).copy(): #avoid dict size changed during iteration error
+        if edge[1] in prior_decisions:
+            cid.remove_edge(*edge)
+
+    #find infopath and control path to a downstream utility
+    downstream_utilities = set([i for i in cid.utilities if D in cid._get_ancestors_of(i)])
+    parents = cid.get_parents(D)
+    other_parents = list(set(parents+[D]) - set([X]))
+    for utility in downstream_utilities:
         info_path = find_active_path_recurse(cid, [D, X], utility, other_parents)
-        if control_path and info_path:
+        if info_path:
+            control_path = find_dirpath(cid, D, utility)
             return {'control':control_path, 'info':info_path}
     return #if no path present
 
@@ -67,14 +80,14 @@ def get_infolinks(cid, path):
     #extract infolinks from a path
     infolinks = []
     for start, end in zip(path[:-1], path[1:]):
-        if end.startswith('D'): #TODO: check whether NullCPD instead
+        if end in cid._get_decisions():
             if start in cid.get_parents(end):
                 infolinks.append(start, end)
     return infolinks
 
 def choose_all_paths(cid, decision, obs):
     paths = {}
-    pair = _get_path_pair(cid, obs, decision)
+    pair = _get_path_pair(cid, decision, obs)
     assert pair is not None, "paths not found from ({}->{}) to {}".format(obs, decision, cid.utilities)
     paths[(obs, decision)] = pair
     infolinks = get_infolinks(cid, pair['control']) + get_infolinks(cid, pair['info'])
@@ -82,10 +95,15 @@ def choose_all_paths(cid, decision, obs):
     while new_infolinks:
         for X, D in new_infolinks:
             if (X,D) not in paths:
-                paths[(X,D)] = _get_path_pair(cid, X, D)
+                paths[(X,D)] = _get_path_pair(cid, D, obs)
         infolinks = get_infolinks(cid, pair['control']) + get_infolinks(cid, pair['info'])
         new_infolinks = set(infolinks) - set(paths)
     return paths
+
+
+#TODO: remove earlier infolinks before finding infopath
+
+
 
 
 #TODO: why does step (1) advise removing all other nodes?
