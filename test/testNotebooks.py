9b1b651724a4a4d50e67eaa5aec0d8e7259ddfba
Tom Everitt <tomeveritt@google.com>
2021-01-17 14:56:35 +0000

    fix notebook tests so they actually test something

--- test/testNotebooks.py
+++ test/testNotebooks.py
@@ -1,20 +1,45 @@
 import unittest
-from testbook import testbook
 import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def run_notebook(notebook_path):
+    full_path = os.path.join(ROOT_DIR, notebook_path)
+    nb_name, _ = os.path.splitext(os.path.basename(full_path))
+
+    with open(full_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    proc.allow_errors = True
+
+    proc.preprocess(nb, {'metadata': {'path': os.path.dirname(full_path)}})
+
+    errors = []
+    for cell in nb.cells:
+        if 'outputs' in cell:
+            for output in cell['outputs']:
+                if output.output_type == 'error':
+                    errors.append(output)
+    return nb, errors
 
 
 class TestNotebooks(unittest.TestCase):
 
     def test_solve_cpd(self):
-        testbook(os.path.join(ROOT_DIR, 'examples/solve_cpd.ipynb'), execute=True)
+        _, errors = run_notebook('examples/solve_cpd.ipynb')
+        self.assertEqual(len(errors), 0)
 
     def test_generate_cpd(self):
-        testbook(os.path.join(ROOT_DIR, 'examples/generate_cid.ipynb'), execute=True)
+        _, errors = run_notebook('examples/generate_cid.ipynb')
+        self.assertEqual(len(errors), 0)
 
     def test_MACID_codebase_demonstration(self):
-        testbook(os.path.join(ROOT_DIR, 'examples/MACID_codebase_demonstration.ipynb'), execute=True)
+        _, errors = run_notebook('examples/MACID_codebase_demonstration.ipynb')
+        self.assertEqual(len(errors), 0)
 
 
 if __name__ == "__main__":
