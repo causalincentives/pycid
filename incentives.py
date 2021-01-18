aa3fd8159c007bdd834c41ef1c3ee563a8637235
Tom Everitt <tomeveritt@google.com>
2021-01-18 13:21:30 +0000

    methods for total and introduced effect

--- incentives.py
+++ incentives.py
@@ -1,21 +1,15 @@
 from typing import List
-
-from pgmpy.inference import BeliefPropagation
-import numpy as np
 from cid import CID
 
 
-from cpd import FunctionCPD
-from fairness import introduced_bias
-
-
-def total_effect(cid: CID, A, X, a1, a2):
+def total_effect(cid: CID, A: str, X: str, a1, a2) -> float:
     "the total effect on X from intervening on A with a2 rather than a1"
-    # TODO handle case when A not root node with interventions
-    return cid.expected_value(X, {A: a2}) - cid.expected_value(X, {A: a1})
+    return cid.expected_value(X, {}, intervene={A: a2}) - \
+           cid.expected_value(X, {}, intervene={A: a1})
 
 
-def introduced_total_effect(cid, A, D, Y, a1, a2):
+def introduced_total_effect(cid: CID, A: str, D: str, Y: str, a1, a2) -> float:
+    """The total introduced effect, comparing the effect of A on D and Y """
     teD = total_effect(cid, A, D, a1, a2)
     teY = total_effect(cid, A, Y, a1, a2)
     return teD - teY
@@ -50,6 +44,6 @@ def trimmed(cid: CID) -> CID:
             for nr in nonrequisite:
                 removed += 1
                 new.remove_edge(nr, decision)
-        if removed==0:
+        if removed == 0:
             break
     return new
--- test/test_incentives.py
+++ test/test_incentives.py
@@ -1,16 +1,41 @@
 import sys, os
-sys.path.insert(0, os.path.abspath('.'))
 import unittest
-import numpy as np
-from examples import get_3node_cid, get_5node_cid, get_5node_cid_with_scaled_utility, get_2dec_cid, get_nested_cid, \
-    get_introduced_bias
+sys.path.insert(0, os.path.abspath('.'))
+from cpd import FunctionCPD
+from incentives import total_effect, introduced_total_effect
+from examples import get_introduced_bias, get_minimal_cid
 
 
 class TestIncentives(unittest.TestCase):
 
     def testTotalEffect(self):
-        pass
-        # TODO
+        cid = get_minimal_cid()
+        self.assertEqual(total_effect(cid, 'A', 'B', 0, 1), 1)
+        cid = get_introduced_bias()
+        self.assertEqual(total_effect(cid, 'A', 'X', 0, 1), 0.5)
+        self.assertEqual(total_effect(cid, 'A', 'D', 0, 1), 0)
+        self.assertEqual(total_effect(cid, 'A', 'Y', 0, 1), 0.5)
+
+    def testIntroducedEffect(self):
+        cid = get_introduced_bias()
+        self.assertEqual(introduced_total_effect(cid, 'A', 'D', 'Y', 0, 1), -0.5)
+        cid.impute_conditional_expectation_decision('D', 'Y')
+        self.assertAlmostEqual(introduced_total_effect(cid, 'A', 'D', 'Y', 0, 1), 0.3333, 2)
+        # Try modified model where X doesn't depend on Z
+        cid = get_introduced_bias()
+        cid.add_cpds(FunctionCPD('X', lambda a, z: a, evidence=['A', 'Z']))
+        cid.impute_conditional_expectation_decision('D', 'Y')
+        self.assertAlmostEqual(introduced_total_effect(cid, 'A', 'D', 'Y', 0, 1), 0, 2)
+        # Try modified model where Y doesn't depend on Z
+        cid = get_introduced_bias()
+        cid.add_cpds(FunctionCPD('Y', lambda x, z: x, evidence=['X', 'Z']))
+        cid.impute_conditional_expectation_decision('D', 'Y')
+        self.assertAlmostEqual(introduced_total_effect(cid, 'A', 'D', 'Y', 0, 1), 0, 2)
+        # Try modified model where Y doesn't depend on X
+        cid = get_introduced_bias()
+        cid.add_cpds(FunctionCPD('Y', lambda x, z: z, evidence=['X', 'Z']))
+        cid.impute_conditional_expectation_decision('D', 'Y')
+        self.assertAlmostEqual(introduced_total_effect(cid, 'A', 'D', 'Y', 0, 1), 0.333, 2)
 
 
 if __name__ == "__main__":
