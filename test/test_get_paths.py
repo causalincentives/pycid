d194a65a4c5d53928081c3316081f7157666cf78
James Fox <james.fox@keble.ox.ac.uk>
2021-01-23 22:41:52 +0000

    fixed some formatting

--- test/test_get_paths.py
+++ test/test_get_paths.py
@@ -5,14 +5,11 @@ import sys, os
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 import unittest
-import numpy as np
-from examples.simple_cids import get_3node_cid, get_5node_cid_with_scaled_utility, get_2dec_cid, \
-    get_sequential_cid
-from examples.story_cids import get_introduced_bias
-from examples.simple_macids import get_basic2agent, get_basic2agent2, get_basic_subgames
+from examples.simple_cids import get_3node_cid
+from examples.simple_macids import get_basic2agent, get_basic_subgames
 from analyze.get_paths import backdoor_path_active_when_conditioning_on_W, find_active_path, get_motifs, get_motif, find_all_dir_paths, find_all_undir_paths, \
-    _directed_decision_free_path, _get_path_structure, path_d_separated_by_Z, frontdoor_indirect_path_not_blocked_by_W, \
-        parents_of_Y_not_descended_from_X
+    _directed_decision_free_path, path_d_separated_by_Z, frontdoor_indirect_path_not_blocked_by_W, \
+    parents_of_Y_not_descended_from_X
 from core.macid import MACID
 
 
@@ -44,18 +41,17 @@ class TestPATHS(unittest.TestCase):
         example = get_basic_subgames()
         self.assertEqual(*find_all_dir_paths(example, 'D11', 'U3'), ['D11', 'D12', 'U3'])
         self.assertFalse(find_all_dir_paths(example, 'U2', 'D2'))
-        self.assertTrue(len(find_all_dir_paths(example, 'D2', 'U2'))==2)
+        self.assertTrue(len(find_all_dir_paths(example, 'D2', 'U2')) == 2)
 
     # @unittest.skip("")
     def test_find_all_undir_paths(self):
         example = get_3node_cid()
-        self.assertTrue(len(find_all_undir_paths(example, 'S', 'U'))==2)        
-        
+        self.assertTrue(len(find_all_undir_paths(example, 'S', 'U')) == 2)
+
         example2 = MACID([
-                    ('X1', 'D'),
-                    ('X2', 'U'),
-                    ],
-                    {1: {'D': ['D'], 'U': ['U']}})
+            ('X1', 'D'),
+            ('X2', 'U')],
+            {1: {'D': ['D'], 'U': ['U']}})
         self.assertEqual(*find_all_undir_paths(example2, 'X1', 'D'), ['X1', 'D'])
         self.assertFalse(find_all_undir_paths(example2, 'X1', 'U'))
 
@@ -71,11 +67,10 @@ class TestPATHS(unittest.TestCase):
     # @unittest.skip("")
     def test_path_d_seperated_by_Z(self):
         example = MACID([
-                    ('X1', 'D'),
-                    ('X2', 'D'),
-                    ('D', 'U')
-                    ],
-                    {1: {'D': ['D'], 'U': ['U']}})   
+            ('X1', 'D'),
+            ('X2', 'D'),
+            ('D', 'U')],
+            {1: {'D': ['D'], 'U': ['U']}})   
         self.assertFalse(path_d_separated_by_Z(example, ['X1', 'D', 'U']))
         self.assertTrue(path_d_separated_by_Z(example, ['X1', 'D', 'U'], ['D']))
         self.assertTrue(path_d_separated_by_Z(example, ['X1', 'D', 'X2']))
@@ -85,29 +80,27 @@ class TestPATHS(unittest.TestCase):
     # @unittest.skip("")
     def test_frontdoor_indirect_path_not_blocked_by_W(self):
         example = MACID([
-                    ('X1', 'X3'),
-                    ('X1', 'D'),
-                    ('X2', 'D'),
-                    ('D', 'U')
-                    ],
-                    {1: {'D': ['D'], 'U': ['U']}})   
+            ('X1', 'X3'),
+            ('X1', 'D'),
+            ('X2', 'D'),
+            ('D', 'U')],
+            {1: {'D': ['D'], 'U': ['U']}})   
         self.assertTrue(frontdoor_indirect_path_not_blocked_by_W(example, 'X2', 'X1', ['D']))
         self.assertFalse(frontdoor_indirect_path_not_blocked_by_W(example, 'X2', 'X1'))
         self.assertFalse(frontdoor_indirect_path_not_blocked_by_W(example, 'X3', 'X1', ['D']))
         self.assertFalse(frontdoor_indirect_path_not_blocked_by_W(example, 'X3', 'X1'))
         self.assertFalse(frontdoor_indirect_path_not_blocked_by_W(example, 'X1', 'U'))
         self.assertFalse(frontdoor_indirect_path_not_blocked_by_W(example, 'X1', 'U', ['D']))
-        
+
     # @unittest.skip("")
     def test_parents_of_Y_not_descended_from_X(self):
         example = MACID([
-                    ('X1', 'X3'),
-                    ('X1', 'D'),
-                    ('X2', 'D'),
-                    ('X2', 'U'),
-                    ('D', 'U')
-                    ],
-                    {1: {'D': ['D'], 'U': ['U']}}) 
+            ('X1', 'X3'),
+            ('X1', 'D'),
+            ('X2', 'D'),
+            ('X2', 'U'),
+            ('D', 'U')],
+            {1: {'D': ['D'], 'U': ['U']}}) 
         self.assertCountEqual(parents_of_Y_not_descended_from_X(example, 'U', 'X1'), ['X2'])
         self.assertCountEqual(parents_of_Y_not_descended_from_X(example, 'U', 'X2'), ['X2'])
         self.assertCountEqual(parents_of_Y_not_descended_from_X(example, 'U', 'X3'), ['D', 'X2'])
@@ -115,20 +108,16 @@ class TestPATHS(unittest.TestCase):
     # @unittest.skip("")
     def test_backdoor_path_active_when_conditioning_on_W(self):
         example = MACID([
-                    ('X1', 'X3'),
-                    ('X1', 'D'),
-                    ('X2', 'D'),
-                    ('X2', 'U'),
-                    ('D', 'U')
-                    ],
-                    {1: {'D': ['D'], 'U': ['U']}}) 
-        example.draw()
+            ('X1', 'X3'),
+            ('X1', 'D'),
+            ('X2', 'D'),
+            ('X2', 'U'),
+            ('D', 'U')],
+            {1: {'D': ['D'], 'U': ['U']}}) 
         self.assertFalse(backdoor_path_active_when_conditioning_on_W(example, 'X3', 'X2'))
         self.assertTrue(backdoor_path_active_when_conditioning_on_W(example, 'X3', 'X2', ['D']))
         self.assertFalse(backdoor_path_active_when_conditioning_on_W(example, 'X1', 'X2'))
         self.assertFalse(backdoor_path_active_when_conditioning_on_W(example, 'X1', 'X2', ['D']))
-
-
 
 
 if __name__ == "__main__":
