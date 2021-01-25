29d3dc4a7b42cc3d1de432479f199a9c807a2778
Tom Everitt <tomeveritt@google.com>
2021-01-25 09:30:14 +0000

    fix flake8 config argument

--- test/check-code.sh
+++ test/check-code.sh
@@ -7,7 +7,7 @@ cd `git rev-parse --show-toplevel`   # go to root directory
 python3 -m mypy . --config-file=test/lint-config.ini
 echo "passed type test"
 
-python3 -m flake8 . --config-file=test/lint-config.ini
+python3 -m flake8 --config=test/lint-config.ini .
 echo "passed lint"
 
 python3 -m unittest
