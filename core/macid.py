8d9fb67fd904f553f6d48368cb59e976089c591f
James Fox <james.fox@keble.ox.ac.uk>
2021-03-02 16:23:42 +0000

    new get_all_pure_spe method

--- core/macid.py
+++ core/macid.py
@@ -86,140 +86,140 @@ class MACID(MACIDBase):
         con_rel = CondensedRelevanceGraph(self)
         return list(nx.topological_sort(con_rel))
 
-    def get_all_pure_spe(self) -> List[List[Tuple[Any, List[Tuple[Any, Any]], Any]]]:
-        """Return all pure policy subgame perfect NE in the MAID when the relevance graph is acyclic"""
-        if not RelevanceGraph(self).is_acyclic():
-            raise Exception('The relevance graph for this (MA)CID is not acyclic and so \
-                        this method cannot be used.')
+    # def get_all_pure_spe(self) -> List[List[Tuple[Any, List[Tuple[Any, Any]], Any]]]:
+    #     """Return all pure policy subgame perfect NE in the MAID when the relevance graph is acyclic"""
+    #     if not RelevanceGraph(self).is_acyclic():
+    #         raise Exception('The relevance graph for this (MA)CID is not acyclic and so \
+    #                     this method cannot be used.')
 
-        solutions = self._pure_spe_finder()
-        spe_arrays = [self._create_spe_array(tree) for tree in solutions]
-        return spe_arrays
+    #     solutions = self._pure_spe_finder()
+    #     spe_arrays = [self._create_spe_array(tree) for tree in solutions]
+    #     return spe_arrays
 
-    def _pure_spe_finder(self) -> List[defaultdict]:
-        """this finds all pure strategy subgame perfect NE when the strategic relevance graph is acyclic
-        - first initialises the maid with uniform random conditional probability distributions at every decision.
-        - then fills up a queue with trees containing each solution
-        - the queue will contain only one entry (tree) if there's only one pure strategy subgame perfect NE"""
-        for dec in self.all_decision_nodes:
-            self.impute_random_decision(dec)  # impute random fully mixed policy to all decision nodes.
+    # def _pure_spe_finder(self) -> List[defaultdict]:
+    #     """this finds all pure strategy subgame perfect NE when the strategic relevance graph is acyclic
+    #     - first initialises the maid with uniform random conditional probability distributions at every decision.
+    #     - then fills up a queue with trees containing each solution
+    #     - the queue will contain only one entry (tree) if there's only one pure strategy subgame perfect NE"""
+    #     for dec in self.all_decision_nodes:
+    #         self.impute_random_decision(dec)  # impute random fully mixed policy to all decision nodes.
 
-        bp = BeliefPropagation(self)
-        print(type(bp))
-        queue = self._instantiate_initial_tree()
+    #     bp = BeliefPropagation(self)
+    #     print(type(bp))
+    #     queue = self._instantiate_initial_tree()
 
-        while not self._stopping_condition(queue):
-            queue = self._reduce_tree_once(queue, bp)
-        return queue
+    #     while not self._stopping_condition(queue):
+    #         queue = self._reduce_tree_once(queue, bp)
+    #     return queue
 
-    def _stopping_condition(self, queue: List[defaultdict]) -> bool:
-        """stopping condition for recursive tree filling"""
-        tree = queue[0]
-        root_node_full = bool(tree[0][0])
-        return root_node_full
+    # def _stopping_condition(self, queue: List[defaultdict]) -> bool:
+    #     """stopping condition for recursive tree filling"""
+    #     tree = queue[0]
+    #     root_node_full = bool(tree[0][0])
+    #     return root_node_full
 
-    def _create_spe_array(self, tree: defaultdict) -> List[Tuple[Any, List[Tuple[Any, Any]], Any]]:
-        """Return the subgame perfect equilibirium in a nested list form
-        Example output: [('D1', [], 0), ('D2', [('D1, 0)], 1), ('D2', [('D1, 1)], 0)]
+    # def _create_spe_array(self, tree: defaultdict) -> List[Tuple[Any, List[Tuple[Any, Any]], Any]]:
+    #     """Return the subgame perfect equilibirium in a nested list form
+    #     Example output: [('D1', [], 0), ('D2', [('D1, 0)], 1), ('D2', [('D1, 1)], 0)]
 
-        The first argument of each triple gives the decision node, the second argument gives the
-        decision context being conditioned on, and the third gives the decision node's action prescribed
-        by the pure SPE.
-        """
-        dec_list = self.get_valid_order()
-        decision_cardinalities = [self.get_cardinality(dec) for dec in dec_list]
+    #     The first argument of each triple gives the decision node, the second argument gives the
+    #     decision context being conditioned on, and the third gives the decision node's action prescribed
+    #     by the pure SPE.
+    #     """
+    #     dec_list = self.get_valid_order()
+    #     decision_cardinalities = [self.get_cardinality(dec) for dec in dec_list]
 
-        spe_array = []
-        for row in range(len(tree) - 1):
-            cols = tree[row].keys()
-            for i in cols:
-                divisor = 1
-                action_values = []
-                for j, dec_card in reversed(list(enumerate(decision_cardinalities[:row]))):
-                    action_values.append((i // divisor) % decision_cardinalities[j])
-                    divisor *= dec_card
-                decision_context_values = list(reversed(action_values))
-                decision_context = list(zip(dec_list[:row], decision_context_values))
-                spe_array.append((dec_list[row], decision_context, tree[row][i][row]))
+    #     spe_array = []
+    #     for row in range(len(tree) - 1):
+    #         cols = tree[row].keys()
+    #         for i in cols:
+    #             divisor = 1
+    #             action_values = []
+    #             for j, dec_card in reversed(list(enumerate(decision_cardinalities[:row]))):
+    #                 action_values.append((i // divisor) % decision_cardinalities[j])
+    #                 divisor *= dec_card
+    #             decision_context_values = list(reversed(action_values))
+    #             decision_context = list(zip(dec_list[:row], decision_context_values))
+    #             spe_array.append((dec_list[row], decision_context, tree[row][i][row]))
 
-        return spe_array
+    #     return spe_array
 
-    def _instantiate_initial_tree(self) -> List[defaultdict]:
-        """Create a tree (a nested dictionary) used for SPE backward induction."""
-        dec_list = self.get_valid_order()
-        decision_cardinalities = [self.get_cardinality(dec) for dec in dec_list]
-        # find number of pure strategies for each decision node (taking into account prev decisions)
-        action_space_list = list(itertools.accumulate(decision_cardinalities, operator.mul))
-        cols_in_each_tree_row = [1] + action_space_list
+    # def _instantiate_initial_tree(self) -> List[defaultdict]:
+    #     """Create a tree (a nested dictionary) used for SPE backward induction."""
+    #     dec_list = self.get_valid_order()
+    #     decision_cardinalities = [self.get_cardinality(dec) for dec in dec_list]
+    #     # find number of pure strategies for each decision node (taking into account prev decisions)
+    #     action_space_list = list(itertools.accumulate(decision_cardinalities, operator.mul))
+    #     cols_in_each_tree_row = [1] + action_space_list
 
-        actions_for_dec_list = []
-        for card in decision_cardinalities:
-            actions_for_dec_list.append(list(range(card)))     # append the range of actions each decion can make
-        # create entry for final row of decision array
-        final_row_actions = list(itertools.product(*actions_for_dec_list))
+    #     actions_for_dec_list = []
+    #     for card in decision_cardinalities:
+    #         actions_for_dec_list.append(list(range(card)))     # append the range of actions each decion can make
+    #     # create entry for final row of decision array
+    #     final_row_actions = list(itertools.product(*actions_for_dec_list))
 
-        tree_initial: defaultdict = defaultdict(dict)   # creates a nested dictionary
-        for i in range(0, len(self.all_decision_nodes) + 1):
-            for j in range(cols_in_each_tree_row[i]):     # initialise tree with empty tuples.
-                tree_initial[i][j] = ()
+    #     tree_initial: defaultdict = defaultdict(dict)   # creates a nested dictionary
+    #     for i in range(0, len(self.all_decision_nodes) + 1):
+    #         for j in range(cols_in_each_tree_row[i]):     # initialise tree with empty tuples.
+    #             tree_initial[i][j] = ()
 
-        for i in range(cols_in_each_tree_row[-1]):
-            tree_initial[len(self.all_decision_nodes)][i] = final_row_actions[i]
+    #     for i in range(cols_in_each_tree_row[-1]):
+    #         tree_initial[len(self.all_decision_nodes)][i] = final_row_actions[i]
 
-        trees_queue = [tree_initial]  # list of all possible decision trees
-        return trees_queue
+    #     trees_queue = [tree_initial]  # list of all possible decision trees
+    #     return trees_queue
 
-    def _reduce_tree_once(self, queue: List[defaultdict], bp: BeliefPropagation) -> List[defaultdict]:
-        """Find first node in tree not yet evaluated using prefix-traversal
-        and then update the tree by evaluating this node - apply this repeatedly
-        until tree is full"""
-        tree = queue.pop(0)
-        for row in range(len(tree) - 2, -1, -1):
-            for col in range(0, len(tree[row])):
-                node_full = bool(tree[row][col])
-                if node_full:
-                    continue
-                else:    # if node is empty => update it by finding maximum children
-                    queue_update = self._max_childen(tree, row, col, queue, bp)
-                    return queue_update
-        return queue  # shouldn't ve called
+    # def _reduce_tree_once(self, queue: List[defaultdict], bp: BeliefPropagation) -> List[defaultdict]:
+    #     """Find first node in tree not yet evaluated using prefix-traversal
+    #     and then update the tree by evaluating this node - apply this repeatedly
+    #     until tree is full"""
+    #     tree = queue.pop(0)
+    #     for row in range(len(tree) - 2, -1, -1):
+    #         for col in range(0, len(tree[row])):
+    #             node_full = bool(tree[row][col])
+    #             if node_full:
+    #                 continue
+    #             else:    # if node is empty => update it by finding maximum children
+    #                 queue_update = self._max_childen(tree, row, col, queue, bp)
+    #                 return queue_update
+    #     return queue  # shouldn't ve called
 
-    def _max_childen(self, tree: defaultdict, row: int, col: int, queue: List[defaultdict],
-                     bp: BeliefPropagation) -> List[defaultdict]:
-        """ Add to the queue the tree(s) filled with the node updated with whichever
-        child(ren) yield the most utilty for the agent making the decision."""
-        dec_list = self.get_valid_order()
-        children_ev = []
-        dec_num_act = self.get_cardinality(dec_list[row])  # num actions (children in the tree) of this decision
+    # def _max_childen(self, tree: defaultdict, row: int, col: int, queue: List[defaultdict],
+    #                  bp: BeliefPropagation) -> List[defaultdict]:
+    #     """ Add to the queue the tree(s) filled with the node updated with whichever
+    #     child(ren) yield the most utilty for the agent making the decision."""
+    #     dec_list = self.get_valid_order()
+    #     children_ev = []
+    #     dec_num_act = self.get_cardinality(dec_list[row])  # num actions (children in the tree) of this decision
 
-        # using col*dec_num_act and (col*dec_num_act)+dec_num_act so we iterate over all of the
-        # agent's considered actions (children in the tree)
-        for indx in range(col * dec_num_act, (col * dec_num_act) + dec_num_act):
-            children_ev.append(self._get_ev(tree[row + 1][indx], row, bp))
-        max_indexes = [i for i, j in enumerate(children_ev) if j == max(children_ev)]
+    #     # using col*dec_num_act and (col*dec_num_act)+dec_num_act so we iterate over all of the
+    #     # agent's considered actions (children in the tree)
+    #     for indx in range(col * dec_num_act, (col * dec_num_act) + dec_num_act):
+    #         children_ev.append(self._get_ev(tree[row + 1][indx], row, bp))
+    #     max_indexes = [i for i, j in enumerate(children_ev) if j == max(children_ev)]
 
-        for i in range(len(max_indexes)):
-            tree[row][col] = tree[row + 1][(col * dec_num_act) + max_indexes[i]]
-            new_tree = copy.deepcopy(tree)
-            queue.append(new_tree)
-        return queue
+    #     for i in range(len(max_indexes)):
+    #         tree[row][col] = tree[row + 1][(col * dec_num_act) + max_indexes[i]]
+    #         new_tree = copy.deepcopy(tree)
+    #         queue.append(new_tree)
+    #     return queue
 
-    def _get_ev(self, dec_instantiation: Tuple[int], row: int, bp: BeliefPropagation) -> float:
-        """Return the expected value of a certain decision node instantiation
-        for the agent making the decision"""
-        dec_list = self.get_valid_order()
-        dec = dec_list[row]
+    # def _get_ev(self, dec_instantiation: Tuple[int], row: int, bp: BeliefPropagation) -> float:
+    #     """Return the expected value of a certain decision node instantiation
+    #     for the agent making the decision"""
+    #     dec_list = self.get_valid_order()
+    #     dec = dec_list[row]
 
-        agent = self.whose_node[dec]      # gets the agent making that decision
-        utils = self.utility_nodes_agent[agent]       # gets the utility nodes for that agent
-        factor = bp.query(variables=utils, evidence=dict(zip(dec_list, dec_instantiation)))
+    #     agent = self.whose_node[dec]      # gets the agent making that decision
+    #     utils = self.utility_nodes_agent[agent]       # gets the utility nodes for that agent
+    #     factor = bp.query(variables=utils, evidence=dict(zip(dec_list, dec_instantiation)))
 
-        ev = 0
-        for idx, prob in np.ndenumerate(factor.values):
-            for i in range(len(utils)):  # account for each agent having multiple utilty nodes
-                if prob != 0:
-                    ev += prob * idx[i]
-        return ev
+    #     ev = 0
+    #     for idx, prob in np.ndenumerate(factor.values):
+    #         for i in range(len(utils)):  # account for each agent having multiple utilty nodes
+    #             if prob != 0:
+    #                 ev += prob * idx[i]
+    #     return ev
 
     def copy_without_cpds(self) -> MACID:
         """copy the MACID structure"""
