46bfcbde64b91aef40e1b3eefbc59c9e1b0f8c6f
James Fox <james.fox@keble.ox.ac.uk>
2021-03-03 13:46:40 +0000

    correct test_examples

--- test/test_examples.py
+++ test/test_examples.py
@@ -27,34 +27,49 @@ class TestExamples(unittest.TestCase):
 
     def test_simple_cid_examples(self) -> None:
         get_minimal_cid()
-        get_5node_cid()
+        get_3node_cid()
         get_5node_cid()
         get_5node_cid_with_scaled_utility()
         get_2dec_cid()
+        get_sequential_cid()
         get_insufficient_recall_cid()
+        get_trim_example_cid()
 
     def test_simple_macid_examples(self) -> None:
-        get_basic2agent_acyclic
-        get_basic2agent_cyclic
-        basic2agent
-        basic2agent_tie_break
-        basic_different_dec_cardinality
-        c2d()
-        sequential()
+        get_basic_subgames()
+        get_basic_subgames2()
+        get_basic_subgames3()
+        get_path_example()
+        basic2agent_tie_break()
+        two_agent_one_pne()
+        two_agent_two_pne()
+        two_agent_no_pne()
+        two_agents_three_actions()
+        basic_different_dec_cardinality()
 
     def test_story_cid_examples(self) -> None:
         get_introduced_bias()
-        get_car_accident_predictor()
         get_fitness_tracker()
+        get_car_accident_predictor()
         get_content_recommender()
+        get_content_recommender2()
         get_modified_content_recommender()
+        get_grade_predictor()
 
     def test_story_macid_examples(self) -> None:
+        prisoners_dilemma()
+        prisoners_dilemma2()
+        battle_of_the_sexes()
+        matching_pennies()
+        taxi_competition()
+        modified_taxi_competition()
         tree_doctor()
+        forgetful_movie_star()
+        subgame_difference()
         road_example()
-        basic2agent_3()
         politician()
         umbrella()
+        sequential()
         signal()
         triage()
 
