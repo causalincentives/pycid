5f67b6d36726040dc6df0c29a7c82187b2669a55
Tom Everitt <tomeveritt@google.com>
2021-03-12 12:52:35 +0000

    add default arguments to ITE and total effect

--- pycid/analyze/effects.py
+++ pycid/analyze/effects.py
@@ -1,14 +1,14 @@
 from pycid.core.cid import CID
 
 
-def total_effect(cid: CID, a: str, x: str, a1: int, a2: int) -> float:
+def total_effect(cid: CID, a: str, x: str, a0: int = 0, a1: int = 1) -> float:
     "the total effect on x from intervening on a with a2 rather than a1"
-    total_effect = cid.expected_value([x], {}, intervene={a: a2})[0] - cid.expected_value([x], {}, intervene={a: a1})[0]
+    total_effect = cid.expected_value([x], {}, intervene={a: a1})[0] - cid.expected_value([x], {}, intervene={a: a0})[0]
     return total_effect  # type: ignore
 
 
-def introduced_total_effect(cid: CID, a: str, d: str, y: str, a1: int, a2: int) -> float:
+def introduced_total_effect(cid: CID, a: str, d: str, y: str, a0: int = 0, a1: int = 1) -> float:
     """The total introduced effect, comparing the effect of a on d and y """
-    te_d = total_effect(cid, a, d, a1, a2)
-    te_y = total_effect(cid, a, y, a1, a2)
+    te_d = total_effect(cid, a, d, a0, a1)
+    te_y = total_effect(cid, a, y, a0, a1)
     return te_d - te_y
