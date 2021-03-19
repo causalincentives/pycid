385ae5474ee889e5adbc0af12b215c3052d26694
James Fox <james.fox@keble.ox.ac.uk>
2021-03-19 17:44:50 +0000

    simplify notebook imports

--- pycid/analyze/__init__.py
+++ pycid/analyze/__init__.py
@@ -1,6 +1,12 @@
 from pycid.analyze.effects import introduced_total_effect, total_effect  # noqa
 from pycid.analyze.instrumental_control_incentive import admits_ici, admits_ici_list  # noqa
-from pycid.analyze.reasoning_patterns import manipulation, revealing_or_denying, signaling  # noqa
+from pycid.analyze.reasoning_patterns import (  # noqa
+    direct_effect,
+    get_reasoning_patterns,
+    manipulation,
+    revealing_or_denying,
+    signaling,
+)
 from pycid.analyze.requisite_graph import requisite, requisite_graph, requisite_list  # noqa
 from pycid.analyze.response_incentive import admits_ri, admits_ri_list  # noqa
 from pycid.analyze.value_of_control import (  # noqa
