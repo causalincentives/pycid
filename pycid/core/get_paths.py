15492e2a8eefdb17b1eb7d5268f38263cc060dc5
James Fox <james.fox@keble.ox.ac.uk>
2021-03-23 00:05:03 +0000

    change type in get_paths to cbn
    should be the most general object possible

--- pycid/core/get_paths.py
+++ pycid/core/get_paths.py
@@ -2,7 +2,7 @@ from typing import Callable, Iterable, Iterator, List, Sequence, Set, Tuple
 
 import networkx as nx
 
-from pycid.core.macid_base import MACIDBase
+from pycid.core.causal_bayesian_network import CausalBayesianNetwork
 
 
 def _dfs_search_paths(start: str, end: str, successors: Callable[[List[str]], Iterable[str]]) -> Iterator[List[str]]:
@@ -33,15 +33,15 @@ def _dfs_search_paths(start: str, end: str, successors: Callable[[List[str]], It
         successor_stack.append(iter(successors(path)))
 
 
-def _active_neighbours(mb: MACIDBase, path: Sequence[str], observed: Set[str]) -> Set[str]:
+def _active_neighbours(cbn: CausalBayesianNetwork, path: Sequence[str], observed: Set[str]) -> Set[str]:
     """Find possibly active extensions of path conditional on the `observed' set of nodes."""
     end_of_path = path[-1]
-    last_forward = len(path) > 1 and end_of_path in mb.get_children(path[-2])
-    possible_colliders: Set[str] = set().union(*[set(mb._get_ancestors_of(e)) for e in observed])  # type: ignore
+    last_forward = len(path) > 1 and end_of_path in cbn.get_children(path[-2])
+    possible_colliders: Set[str] = set().union(*[set(cbn._get_ancestors_of(e)) for e in observed])  # type: ignore
 
     # if going upward or at a possible collider, it's possible to continue to a parent
     if not last_forward or end_of_path in possible_colliders:
-        active_parents = set(mb.get_parents(end_of_path)) - observed
+        active_parents = set(cbn.get_parents(end_of_path)) - observed
     else:
         active_parents = set()
 
@@ -49,25 +49,27 @@ def _active_neighbours(mb: MACIDBase, path: Sequence[str], observed: Set[str]) -
     if end_of_path in observed:
         active_children = set()
     else:
-        active_children = set(mb.get_children(end_of_path))
+        active_children = set(cbn.get_children(end_of_path))
 
     active_neighbours = active_parents.union(active_children)
     new_active_neighbours = active_neighbours - set(path)
     return new_active_neighbours
 
 
-def find_active_path(mb: MACIDBase, start_node: str, end_node: str, observed: Set[str] = set()) -> List[str]:
+def find_active_path(
+    cbn: CausalBayesianNetwork, start_node: str, end_node: str, observed: Set[str] = set()
+) -> List[str]:
     """Find an active path from `start_node' to `end_node' given the `observed' set of nodes."""
     considered_nodes = observed.union({start_node}, {end_node})
     for node in considered_nodes:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     if end_node in observed:
         raise ValueError("No active path")
 
     def successors(path: List[str]) -> Set[str]:
-        return _active_neighbours(mb, path, observed)
+        return _active_neighbours(cbn, path, observed)
 
     try:
         return next(_dfs_search_paths(start_node, end_node, successors))
@@ -75,13 +77,13 @@ def find_active_path(mb: MACIDBase, start_node: str, end_node: str, observed: Se
         raise ValueError("No active path")
 
 
-def get_motif(mb: MACIDBase, path: Sequence[str], idx: int) -> str:
+def get_motif(cbn: CausalBayesianNetwork, path: Sequence[str], idx: int) -> str:
     """
     Classify three node structure as a forward (chain), backward (chain), fork,
     collider, or endpoint at index 'idx' along path.
     """
     for node in path:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     if idx > len(path) - 1:
@@ -90,83 +92,83 @@ def get_motif(mb: MACIDBase, path: Sequence[str], idx: int) -> str:
     if len(path) == idx + 1:
         return "endpoint"
 
-    elif mb.has_edge(path[idx - 1], path[idx]) and mb.has_edge(path[idx], path[idx + 1]):
+    elif cbn.has_edge(path[idx - 1], path[idx]) and cbn.has_edge(path[idx], path[idx + 1]):
         return "forward"
 
-    elif mb.has_edge(path[idx + 1], path[idx]) and mb.has_edge(path[idx], path[idx - 1]):
+    elif cbn.has_edge(path[idx + 1], path[idx]) and cbn.has_edge(path[idx], path[idx - 1]):
         return "backward"
 
-    elif mb.has_edge(path[idx - 1], path[idx]) and mb.has_edge(path[idx + 1], path[idx]):
+    elif cbn.has_edge(path[idx - 1], path[idx]) and cbn.has_edge(path[idx + 1], path[idx]):
         return "collider"
 
-    elif mb.has_edge(path[idx], path[idx - 1]) and mb.has_edge(path[idx], path[idx + 1]):
+    elif cbn.has_edge(path[idx], path[idx - 1]) and cbn.has_edge(path[idx], path[idx + 1]):
         return "fork"
 
     else:
         raise RuntimeError(f"unsure how to classify this path at index {idx}")
 
 
-def get_motifs(mb: MACIDBase, path: Sequence[str]) -> List[str]:
+def get_motifs(cbn: CausalBayesianNetwork, path: Sequence[str]) -> List[str]:
     """classify the motif of all nodes along a path as a forward (chain), backward (chain), fork,
     collider or endpoint"""
     for node in path:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     shapes = []
     for i in range(len(path)):
         if i == 0:
-            if path[i] in mb.get_parents(path[i + 1]):
+            if path[i] in cbn.get_parents(path[i + 1]):
                 shapes.append("forward")
             else:
                 shapes.append("backward")
         else:
-            shapes.append(get_motif(mb, path, i))
+            shapes.append(get_motif(cbn, path, i))
     return shapes
 
 
-def find_all_dir_paths(mb: MACIDBase, start_node: str, end_node: str) -> Iterator[List[str]]:
+def find_all_dir_paths(cbn: CausalBayesianNetwork, start_node: str, end_node: str) -> Iterator[List[str]]:
     """Iterate over all directed paths from start node to end node that exist in the (MA)CID."""
     for node in [start_node, end_node]:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     def successors(path: List[str]) -> Iterable[str]:
-        return mb.get_children(path[-1])  # type: ignore
+        return cbn.get_children(path[-1])  # type: ignore
 
     return _dfs_search_paths(start_node, end_node, successors)
 
 
-def find_all_undir_paths(mb: MACIDBase, start_node: str, end_node: str) -> Iterable[List[str]]:
+def find_all_undir_paths(cbn: CausalBayesianNetwork, start_node: str, end_node: str) -> Iterable[List[str]]:
     """
     Finds all undirected paths from start node to end node that exist in the (MA)CID.
     """
     for node in [start_node, end_node]:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     def successors(path: List[str]) -> Iterable[str]:
-        neighbours = set(mb.get_children(path[-1]))
-        neighbours.update(mb.get_parents(path[-1]))
+        neighbours = set(cbn.get_children(path[-1]))
+        neighbours.update(cbn.get_parents(path[-1]))
         neighbours.difference_update(path)
         return neighbours
 
     return _dfs_search_paths(start_node, end_node, successors)
 
 
-def directed_decision_free_path(mb: MACIDBase, start_node: str, end_node: str) -> bool:
+def directed_decision_free_path(cbn: CausalBayesianNetwork, start_node: str, end_node: str) -> bool:
     """
     Checks to see if a directed decision free path exists
     """
     for node in [start_node, end_node]:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     # ignore path's start_node and end_node
-    return any(mb.decisions.isdisjoint(path[1:-1]) for path in find_all_dir_paths(mb, start_node, end_node))
+    return any(cbn.decisions.isdisjoint(path[1:-1]) for path in find_all_dir_paths(cbn, start_node, end_node))
 
 
-def _get_path_edges(mb: MACIDBase, path: Sequence[str]) -> List[Tuple[str, str]]:
+def _get_path_edges(cbn: CausalBayesianNetwork, path: Sequence[str]) -> List[Tuple[str, str]]:
     """
     Returns the structure of a path's edges as a list of pairs.
     In each pair, the first argument states where an edge starts and the second argument states
@@ -175,14 +177,14 @@ def _get_path_edges(mb: MACIDBase, path: Sequence[str]) -> List[Tuple[str, str]]
     """
     structure = []
     for i in range(len(path) - 1):
-        if path[i] in mb.get_parents(path[i + 1]):
+        if path[i] in cbn.get_parents(path[i + 1]):
             structure.append((path[i], path[i + 1]))
-        elif path[i + 1] in mb.get_parents(path[i]):
+        elif path[i + 1] in cbn.get_parents(path[i]):
             structure.append((path[i + 1], path[i]))
     return structure
 
 
-def is_active_path(mb: MACIDBase, path: Sequence[str], observed: Set[str] = None) -> bool:
+def is_active_path(cbn: CausalBayesianNetwork, path: Sequence[str], observed: Set[str] = None) -> bool:
     """
     Check if a specifc path remains active given the 'observed' set of variables.
     """
@@ -190,20 +192,20 @@ def is_active_path(mb: MACIDBase, path: Sequence[str], observed: Set[str] = None
         observed = set()
     considered_nodes = set(path).union(observed)
     for node in considered_nodes:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
     if len(path) < 3:
         return True
 
     for _, b, _ in zip(path[:-2], path[1:-1], path[2:]):
-        structure = get_motif(mb, path, path.index(b))
+        structure = get_motif(cbn, path, path.index(b))
 
         if structure in {"fork", "forward", "backward"} and b in observed:
             return False
 
         if structure == "collider":
-            descendants = nx.descendants(mb, b).union({b})
+            descendants = nx.descendants(cbn, b).union({b})
             if not descendants.intersection(observed):
                 return False
 
@@ -211,7 +213,7 @@ def is_active_path(mb: MACIDBase, path: Sequence[str], observed: Set[str] = None
 
 
 def is_active_indirect_frontdoor_trail(
-    mb: MACIDBase, start_node: str, end_node: str, observed: Set[str] = None
+    cbn: CausalBayesianNetwork, start_node: str, end_node: str, observed: Set[str] = None
 ) -> bool:
     """
     checks whether an active indirect frontdoor path exists given the 'observed' set of variables.
@@ -223,13 +225,13 @@ def is_active_indirect_frontdoor_trail(
         observed = set()
     considered_nodes = observed.union({start_node}, {end_node})
     for node in considered_nodes:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
-    for path in find_all_undir_paths(mb, start_node, end_node):
-        is_frontdoor_path = path[0] in mb.get_parents(path[1])
-        not_blocked_by_observed = is_active_path(mb, path, observed)
-        contains_collider = "collider" in get_motifs(mb, path)
+    for path in find_all_undir_paths(cbn, start_node, end_node):
+        is_frontdoor_path = path[0] in cbn.get_parents(path[1])
+        not_blocked_by_observed = is_active_path(cbn, path, observed)
+        contains_collider = "collider" in get_motifs(cbn, path)
         # default is False since if w = [], any unobserved collider blocks path
         if is_frontdoor_path and not_blocked_by_observed and contains_collider:
             return True
@@ -237,7 +239,9 @@ def is_active_indirect_frontdoor_trail(
         return False
 
 
-def is_active_backdoor_trail(mb: MACIDBase, start_node: str, end_node: str, observed: Set[str] = None) -> bool:
+def is_active_backdoor_trail(
+    cbn: CausalBayesianNetwork, start_node: str, end_node: str, observed: Set[str] = None
+) -> bool:
     """
     Returns true if there is a backdoor path that's active given the 'observed' set of nodes.
     - A backdoor path between X and Z is an (undirected) path in which the first edge goes into the first node (X←···Z)
@@ -246,13 +250,13 @@ def is_active_backdoor_trail(mb: MACIDBase, start_node: str, end_node: str, obse
         observed = set()
     considered_nodes = observed.union({start_node}, {end_node})
     for node in considered_nodes:
-        if node not in mb.nodes():
+        if node not in cbn.nodes():
             raise KeyError(f"The node {node} is not in the (MA)CID")
 
-    for path in find_all_undir_paths(mb, start_node, end_node):
+    for path in find_all_undir_paths(cbn, start_node, end_node):
         if len(path) > 1:  # must have path of at least 2 nodes
-            is_backdoor_path = path[1] in mb.get_parents(path[0])
-            not_blocked_by_observed = is_active_path(mb, path, observed)
+            is_backdoor_path = path[1] in cbn.get_parents(path[0])
+            not_blocked_by_observed = is_active_path(cbn, path, observed)
             if is_backdoor_path and not_blocked_by_observed:
                 return True
     else:
