59f05bc8a556da3c17de7a0e48a1c627e4ecfc0f
Tom Everitt <tomeveritt@google.com>
2021-03-31 12:00:24 +0100

    better random cpd

--- tests/test_causal_bayesian_network.py
+++ tests/test_causal_bayesian_network.py
@@ -6,11 +6,10 @@ import numpy as np
 import pytest
 from pgmpy.factors.discrete import TabularCPD  # type: ignore
 
+from pycid import CID, MACID, CausalBayesianNetwork, RandomCPD
 from pycid.examples.simple_cbns import get_3node_cbn
 from pycid.examples.simple_cids import get_3node_cid, get_minimal_cid
 from pycid.examples.story_macids import taxi_competition
-
-from pycid import CID, MACID, CausalBayesianNetwork, random_cpd
 
 
 @pytest.fixture
@@ -61,7 +60,7 @@ class TestQuery:
     @staticmethod
     def test_query_disconnected_components() -> None:
         cbn = CausalBayesianNetwork([("A", "B")])
-        cbn.add_cpds(random_cpd("A"), random_cpd("B"))
+        cbn.add_cpds(RandomCPD("A"), RandomCPD("B"))
         cbn.query(["A"], {}, intervention={"B": 0})  # the intervention separates A and B into separare components
 
     @staticmethod
--- tests/test_random.py
+++ tests/test_random.py
@@ -1,8 +1,9 @@
 import sys
 
-import pytest
 import networkx as nx
+import pytest
 
+from pycid import CausalBayesianNetwork, FunctionCPD, RandomCPD
 from pycid.random.random_cid import random_cid, random_cids
 from pycid.random.random_dag import random_dag
 
@@ -20,6 +21,16 @@ def test_random_cids_create_one() -> None:
 def test_random_dag_create_one() -> None:
     dag = random_dag(number_of_nodes=5, edge_density=0.4, max_in_degree=4)
     assert nx.is_directed_acyclic_graph(dag)
+
+
+def test_random_cpd() -> None:
+    cbn = CausalBayesianNetwork([("Y", "A"), ("Y", "D")])
+    cbn.add_cpds(
+        RandomCPD("Y"),
+        FunctionCPD("A", lambda y: y),
+        FunctionCPD("D", lambda y: y),
+    )
+    assert cbn.expected_value(["D"], {}, intervention={"A": 0}) == cbn.expected_value(["D"], {}, intervention={"A": 1})
 
 
 if __name__ == "__main__":
