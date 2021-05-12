a52c0317eb7ac94252ea1836e545a9a8cac17ca6
James Fox <james.fox@keble.ox.ac.uk>
2021-05-12 22:38:28 +0100

    update notebooks

--- pycid/core/__init__.py
+++ pycid/core/__init__.py
@@ -1,6 +1,14 @@
 from pycid.core.causal_bayesian_network import CausalBayesianNetwork  # noqa
 from pycid.core.cid import CID  # noqa
-from pycid.core.cpd import DecisionDomain, StochasticFunctionCPD  # noqa
+from pycid.core.cpd import (  # noqa
+    ConstantCPD,
+    DecisionDomain,
+    StochasticFunctionCPD,
+    bernoulli,
+    discrete_uniform,
+    function_copy,
+    noisy_copy,
+)
 from pycid.core.macid import MACID  # noqa
 from pycid.core.macid_base import MACIDBase, MechanismGraph  # noqa
 from pycid.core.relevance_graph import CondensedRelevanceGraph, RelevanceGraph  # noqa
